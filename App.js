d91b771352277a6c482576398d4e4df6a81ba7e8
NicolasAnglade63 <100127990+NicolasAnglade63@users.noreply.github.com>
2022-12-11 22:17:27 +0100

    Setup du projet en React Native
    Creation du dossier de projet (on pourra le renommer plus tard)

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Bonjour les noobs comment ça va!</Text>
+      <Text>React go brrr</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
