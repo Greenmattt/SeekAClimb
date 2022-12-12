7cb912173d51a08b5a701aef20bc696f032a7ec4
NicolasAnglade63 <100127990+NicolasAnglade63@users.noreply.github.com>
2022-12-12 09:32:22 +0100

    Premiers test sur les fichier js
    App.js :
    - Teste de classes
    - import de fichiers externes
    - essaie des <Separator/>
    
    2emepage.js :
    - Textes

---
+++ 2emepage.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Hello = () => {
+    <View>
+        <Text>Bonjout je suis une autre page</Text>
+        <Text>input : </Text>
+        <TextInput defaultValue="Placeholder"/>
+    </View>
+}
+
+module.exports = Hello;
--- App.js
+++ App.js
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+/* il va falloir installer des librairies supplémentaires pour naviguer entre plusieurs écrans
+   je vous rappelle qu'il faut une page par voie*/
+
+/* les commandes :
+npm install @react-navigation/native @react-navigation/native-stack
+npm install react-native-screens react-native-safe-area-context
+*/
+
+//import {NavigationContainer} from '@react-navigation/native'
+
+class Maclasse {
+  constructor(a,b) {
+    this.taille = a*b;
+  }
+  get taille() {
+    return this.taille
+  }
+  changer_aire(a,b) {
+    this.taille = a*b;
+  }
+}
+
+const Hellooo = require("./2emepage.js");
 
 export default function App() {
+  var r = Maclasse(5,2);
   return (
     <View style={styles.container}>
       <Text>Bonjour les noobs comment ça va!</Text>
       <Text>React go brrr</Text>
+      <Button
+      onPress = {() => alert.alert("button")}
+      />
+      <Separator />
+      <Hellooo/>
+      <Separator/>
+      <Text> {"test de class : " + toString(r.taille)} </Text>
       <StatusBar style="auto" />
     </View>
   );
