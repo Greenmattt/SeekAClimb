62af7fd1a025b87f67bda8923d050d690cf3d8f9
Thechalk <100965196+Thechalk@users.noreply.github.com>
2022-12-15 19:19:49 +0100

    Test des bouttons
    en vrai c'est marrant on peut faire des jolis trucs mais bordel tu peux pas lacher la doc des yeux

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Button, Separator, Alert, TextInput} from 'react-native';
+import { StyleSheet, Text, View , Button, Separator, Alert, TextInput, Linking, TouchableOpacity} from 'react-native';
 import React, { useState } from 'react';
 
 // Voila comment on import une classe d'un fichier externe : 
 import Hello from './2emepage';
-
-//import * as React from "react";
 
 /* il va falloir installer des librairies supplémentaires pour naviguer entre plusieurs écrans
    je vous rappelle qu'il faut une page par voie*/
@@ -15,7 +13,7 @@ npm install @react-navigation/native @react-navigation/native-stack
 npm install react-native-screens react-native-safe-area-context
 */
 
-import {NavigationContainer} from '@react-navigation/native';
+import {Link, NavigationContainer} from '@react-navigation/native';
 
 class Rectangle {
   longueur;
@@ -45,19 +43,32 @@ const App = () => {
   const [text, setText] = useState('');
   const r = 42;
   const rectanglllle = new Rectangle(2,32);
+
+  // ça c'est juste pour la variable qui compte les clics
+  let [count, setCount] = useState(0);
+  // ça c'est la fonction qui va avec
+  const onPress = () => setCount(prevCount => prevCount + 1);
+
   return (
     // tout ce qu'il y a dans le return est affiché (en tout cas entre les <View>)
     <View style={styles.container}>
+      {/* tout ça c'est du texte basique*/}
       <Text style={styles.innerText}>Bonjour les noobs comment ça va!</Text>
       <Text style={styles.innerText}>React go brrr et aussi {test(r)}</Text>
-      <Text style={styles.innerText}>La taille du rectangle est :{rectanglllle.area} </Text>
+
+      {/* ça ce sont les boutons classics où tu peux pas faire grand chose niveau style */}
+      <Button color="#121212" title="clic bouffon" onPress={ ()=>{ Linking.openURL('https://thomann.de')}}/>
+      <Button title="Add 1" onPress={onPress}/>
+
+      {/* et là on commence à arriver vers des boutons qui ressemblent à quelquechose */}
+      <TouchableOpacity activeOpacity={0.1} style={styles.button}><Text style={styles.button}>Bonjour</Text></TouchableOpacity>
+
+      <Text style={styles.innerText}>clics : {count}</Text>
+      <Text style={styles.innerText}>La taille du rectangle est :{rectanglllle.area}</Text>
       
       {/* Hello vient du fichier 2emepage.js */}
       <Hello/>
 
-      {/* le input qui marche cette fois */}
-
-       
       <TextInput style={styles.innerText}
                 onChangeText={setText}
                 defaultValue={text}
@@ -81,6 +92,11 @@ const styles = StyleSheet.create({
   },
   innerText: {
     color: '#ffffff',
+  },
+  button: {
+    backgroundColor: "orange",
+    padding: 2,
+    color: "pink",
   }
 });
 
