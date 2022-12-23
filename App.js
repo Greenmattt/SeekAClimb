48de76c5373fb82deedae05d9956927d8f1541bd
Thechalk <100965196+Thechalk@users.noreply.github.com>
2022-12-23 16:43:46 +0100

    ajout d'un navbar et supression de la tentative de nico

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { Text, View, TouchableOpacity, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React, { useCallback, useEffect, useState } from 'react';
 import Entypo from '@expo/vector-icons/Entypo';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
 // commande à faire : npx expo install expo-splash-screen
+// pour la nav bar : npm install @react-navigation/bottom-tabs
 
 // je préviens toutes les fonctions compliquées pour le splash screen sortent de cette doc : 
 // https://docs.expo.dev/versions/latest/sdk/splash-screen/
 // https://hackernoon.com/how-to-design-a-splash-screen-with-expo-and-react-native
 
-import Barre from './Footer';
+// import Barre from './Footer';
 
 
 // permet de garder le splash screen visible jusqu'à ce que toutes les ressources soient téléchargées
@@ -50,20 +53,43 @@ const App =() => {
     return null;
   }
 
-// fin du setup du splash screen
+  // fin du setup du splash screen
 
+  // Page principale
+  function HomeScreen() {
+    return (
+      <View style={styleMain(false).fond} onLayout={onLayoutRootView}>
+        <Text style={styleMain(false).text}>page d'entrée</Text>
+      </View>
+    );
+  }
+
+  function SettingsScreen() {
+    return (
+      <View style={styleMain(false).fond}>
+        <Text style={styleMain(false).text}>page des paramètres</Text>
+      </View>
+    );
+  }
+
+  function AccountScreen() {
+    return (
+      <View style={styleMain(false).fond}>
+        <Text style={styleMain(false).text}>page du compte</Text>
+      </View>
+    );
+  }
+
+  const Tab = createBottomTabNavigator();
 
   return (
-    <View style={{flex:1}}>
-      {/* La partie principale de l'application (encore tout à faire) */}
-      <View style={styleMain(true).fond} onLayout={onLayoutRootView}>
-        <StatusBar style="auto" />
-        <Text>Écran principal</Text>
-      </View>
-
-      {/* Un essai de 'nav' en bas où on peut choisir facilement sur quelle page aller (oui les images sont à changer) */}
-      <Barre/>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen}/>
+        <Tab.Screen name="Settings" component={SettingsScreen}/>
+        <Tab.Screen name="Account" component={AccountScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -74,7 +100,7 @@ var styleMain = function(clair) {
 
   return {
     fond: {
-      flex: 9,
+      flex: 1,
       backgroundColor: couleurF, 
       alignItems: 'center',
       justifyContent: 'space-evenly',
