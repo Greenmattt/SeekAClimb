a96d4b39b76ca21337a34e5580a3dec9bcbe4ecc
Thechalk <100965196+Thechalk@users.noreply.github.com>
2023-01-18 12:04:31 +0100

    update settings

--- Settings.js
+++ Settings.js
@@ -1,27 +1,60 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Text, View, Switch } from 'react-native';
 
 const Settings = () => {
+
+  const [darkTheme, onChangeDarkTheme] = useState(false);
+  const toggleSwitch = () => onChangeDarkTheme(a => !a)
+
   return (
     <View style={styleMain(false).fond}>
-      <Text style={styleMain(false).text}>Page des paramÃ¨tres</Text>
+      <View style={styleMain(false).header}>
+        <Text style = {styleMain(false).text}>Page des Paramètres</Text>
+      </View>
+      <View style = {styleMain(false).container}>
+        <Text style = {styleMain(false).text2}>Theme Sombre</Text>
+
+        <Switch
+        trackColor={{false: '#3a75b1', true: "#3ab175"}}
+        thumbColor = {darkTheme ? "#fff" : "#fff"}
+        onValueChange = {toggleSwitch}
+        value = {darkTheme}/>
+
+        <Text style = {styleMain(false).text2}>Theme Clair</Text>
+      </View>
     </View>
   );
 }
 
 var styleMain = function(clair) {
-    var couleurF = clair ? "#FFFFFF" : "#000000";
-    var couleurT = clair ? "#000000" : "#FFFFFF";
+    var couleurF = clair ? "#FFFFFF" : "#131514";
+    var couleurT = clair ? "#131514" : "#FFFFFF";
   
     return {
       fond: {
         flex: 1,
-        backgroundColor: couleurF, 
-        alignItems: 'center',
+        backgroundColor: couleurF,
+        padding: 10,
+      },
+      container: {
+        padding: 5,
+        flex: 10,
+        flexDirection: 'row',
         justifyContent: 'space-evenly',
+        backgroundColor: '#282828',
+        borderRadius: 5,
+      },
+      header: {
+        flex: 1,
       },
       text: {
         color: couleurT,
+        fontSize: 20,
+        textAlign: 'center',
+      },
+      text2: {
+        color: couleurT,
+        textAlign: 'center',
       },
       icon: {
         width: 30,
