83f4c3cc055bd5d67c2a648321618c811586b160
Greenmattt <100964076+Greenmattt@users.noreply.github.com>
2023-01-25 12:06:10 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,131 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { Text, View, TouchableOpacity, Image } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import React, { useCallback, useEffect, useState } from 'react';
-import Entypo from '@expo/vector-icons/Entypo';
-import * as SplashScreen from 'expo-splash-screen';
-import * as Font from 'expo-font';
-// commande à faire : npx expo install expo-splash-screen
-// pour la nav bar : npm install @react-navigation/material-top-tabs react-native-tab-view
-// puis : npx expo install react-native-pager-view
+const styles = StyleSheet.create({
+  main:{
+    flex:1,
+    backgroundColor : '#131514',
+  },
+  
+  text: {
+    fontSize: 40,
+    color: 'white',
+    textAlign: 'center',
+    flex: 1,
+    textTransform: 'capitalize',
+    fontStyle: 'italic',
+  },
 
-// je préviens toutes les fonctions compliquées pour le splash screen sortent de cette doc : 
-// https://docs.expo.dev/versions/latest/sdk/splash-screen/
-// https://hackernoon.com/how-to-design-a-splash-screen-with-expo-and-react-native
+  header:{
+    flex: 1,
+    backgroundColor: '#214E34'
+  },
 
-import Settings from './Settings';
-import Account from './Account';
-import Routes from './Routes';
-import Home from './Home';
+  headText: {
+    fontSize: 20,
+    color: 'white',
+    textAlign: 'center',
+    fontStyle: 'italic',
+  },
 
+  container: {
+    flex: 15,
+    justifyContent: 'space-between',
+    backgroundColor: '#131514',
+  },
 
+  top: {
+    flex: 20,
+    backgroundColor: '#282828',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
 
-// permet de garder le splash screen visible jusqu'à ce que toutes les ressources soient téléchargées
-SplashScreen.preventAutoHideAsync();
+  mid: {
+    flex: 20,
+    backgroundColor: '#282828',
+    borderColor: '#364156',
+    borderWidth: 1,
+    borderRadius: 5,
+  },
 
-const App =() => {
-  const [appIsReady, setAppIsReady] = useState(false);
-
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // Chargement de la police, c'est là qu'on fait nos appels à la base de donnée si besoin
-        await Font.loadAsync(Entypo.font);
-        // Ici on met un timer de 2 secondes artificelle 
-        // /!\ A ENELEVER QUAND ON FERA DES TRUCS UTILES ICI /!\
-        await new Promise(resolve => setTimeout(resolve, 2000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Quand tout est fini on peut render l'application
-        setAppIsReady(true);
-      }
-    }
-
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      // Ici ça permet de cacher le Splash screen, et on le fait pas avant le 
-      // setAppIsReady on pourrait avoir des pb
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
-    return null;
-  }
-
-  // fin du setup du splash screen
-
-  // Page principale
-  function HomeScreen() {
-    return (
-      <Home/>
-    );
-  }
-
-  function RoutesScreen() {
-    return (
-      <Routes/>
-    );
-  }
-
-  function SettingsScreen() {
-    return (
-      <Settings/>
-    );
-  }
-
-  function AccountScreen() {
-    return (
-      <Account/>
-    );
-  }
-
-  function imageSettings () {return(<Image style={styleMain().icon} source={require('./assets/settings_icon.png')}/>)}
-  function imageAccueil () {return(<Image style={styleMain().icon} source={require('./assets/home_icon.png')}/>)}
-  function imageCompte () {return(<Image style={styleMain().icon} source={require('./assets/user_icon.png')}/>)}
-  function imageVoies () {return(<Image style={styleMain().icon} source={require('./assets/Sports_Climbing_icon.png')}/>)}
-  const Tab = createMaterialTopTabNavigator();
-
-  return (
-    <NavigationContainer>
-      <Tab.Navigator tabBarPosition='bottom'>
-        <Tab.Screen name="Home" component={HomeScreen} options={{headerShown:false, tabBarIcon:imageAccueil, tabBarShowIcon:true}}/>
-        <Tab.Screen name="Voies" component={RoutesScreen} options={{headerShown:false, tabBarIcon:imageVoies, tabBarShowIcon:true}}/>
-        <Tab.Screen name="Settings" component={SettingsScreen} options={{headerShown:false, tabBarIcon:imageSettings, tabBarShowIcon:true}}/>
-        <Tab.Screen name="Account" component={AccountScreen} options={{headerShown:false, tabBarIcon:imageCompte, tabBarShowIcon:true}}/>
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
-
-// clair est un booléen
-var styleMain = function(clair) {
-  var couleurF = clair ? "#FFFFFF" : "#000000";
-  var couleurT = clair ? "#000000" : "#FFFFFF";
-
-  return {
-    fond: {
-      flex: 1,
-      backgroundColor: couleurF, 
-      alignItems: 'center',
-      justifyContent: 'space-evenly',
-    },
-    text: {
-      color: couleurT,
-    },
-    icon: {
-      width: 30,
-      height: 30,
-      resizeMode: 'contain',
-    },
-  }
-};
-
-
-export default App;
+  bottom: {
+    flex: 20,
+    backgroundColor: '#282828',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    borderColor: '#364156',
+    borderWidth: 1,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+  },
+});
