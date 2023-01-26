b75e17d9eaa2beccfc1cb902fb26149d570ace47
NicolasAnglade63 <100127990+NicolasAnglade63@users.noreply.github.com>
2023-01-26 09:34:43 +0100

    Update App.js
    Suppression du splash screen qui cassait tout chez moi

--- App.js
+++ App.js
@@ -1,67 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { Text, View, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React, { useCallback, useEffect, useState } from 'react';
-import Entypo from '@expo/vector-icons/Entypo';
-import * as SplashScreen from 'expo-splash-screen';
-import * as Font from 'expo-font';
-// commande à faire : npx expo install expo-splash-screen
+
 // pour la nav bar : npm install @react-navigation/material-top-tabs react-native-tab-view
 // puis : npx expo install react-native-pager-view
 
-// je préviens toutes les fonctions compliquées pour le splash screen sortent de cette doc : 
-// https://docs.expo.dev/versions/latest/sdk/splash-screen/
-// https://hackernoon.com/how-to-design-a-splash-screen-with-expo-and-react-native
 
 import Settings from './Settings';
 import Account from './Account';
 import Routes from './Routes';
 import Home from './Home';
 
-const Stack = createNativeStackNavigator(); 
-
-
 // permet de garder le splash screen visible jusqu'à ce que toutes les ressources soient téléchargées
-SplashScreen.preventAutoHideAsync();
-
-const App =() => {
-  const [appIsReady, setAppIsReady] = useState(false);
-
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // Chargement de la police, c'est là qu'on fait nos appels à la base de donnée si besoin
-        await Font.loadAsync(Entypo.font);
-        // Ici on met un timer de 2 secondes artificelle 
-        // /!\ A ENELEVER QUAND ON FERA DES TRUCS UTILES ICI /!\
-        await new Promise(resolve => setTimeout(resolve, 2000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Quand tout est fini on peut render l'application
-        setAppIsReady(true);
-      }
-    }
-
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      // Ici ça permet de cacher le Splash screen, et on le fait pas avant le 
-      // setAppIsReady on pourrait avoir des pb
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
-    return null;
-  }
-
-  // fin du setup du splash screen
+const App = () => {
 
   // Page principale
   function HomeScreen() {
