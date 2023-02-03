315a8b909d9e84e0e419c3cbb930651ea2e61f9f
Thechalk <100965196+Thechalk@users.noreply.github.com>
2023-02-03 14:42:02 +0100

    Update Home.js

--- Home.js
+++ Home.js
@@ -4,13 +4,10 @@ import styles from './Component/Styles'
 
 const Home = () => {
   return (
-    <View style={styles.main}>
       <View style={styles.container}>
         <Text style={styles.textTitre}> Main Menudo </Text>
       </View>
-    </View>
   );
 };
 
 export default Home;
-
