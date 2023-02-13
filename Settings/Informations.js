dec8600deace8fe07bbf918e09539741437fb9d6
Greenmattt <100964076+Greenmattt@users.noreply.github.com>
2023-02-13 02:39:50 +0100

    K C

--- Settings/Informations.js
+++ Settings/Informations.js
@@ -8,42 +8,4 @@ const Informations = () => {
   );
 }
 
-var styleMain = function(clair) {
-    var couleurF = clair ? "#FFFFFF" : "#131514";
-    var couleurT = clair ? "#131514" : "#FFFFFF";
-  
-    return {
-      fond: {
-        flex: 1,
-        backgroundColor: couleurF,
-        padding: 10,
-      },
-      container: {
-        padding: 5,
-        flex: 10,
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        backgroundColor: '#282828',
-        borderRadius: 5,
-      },
-      header: {
-        flex: 1,
-      },
-      text: {
-        color: couleurT,
-        fontSize: 20,
-        textAlign: 'center',
-      },
-      text2: {
-        color: couleurT,
-        textAlign: 'center',
-      },
-      icon: {
-        width: 30,
-        height: 30,
-        resizeMode: 'stretch',
-      },
-    }
-  };
-
 export default Informations;
