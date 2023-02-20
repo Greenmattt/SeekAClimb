d9d92d6ca59f46e543e17e84bcdea5e0391efd96
Thechalk <100965196+Thechalk@users.noreply.github.com>
2023-02-20 09:51:12 +0100

    Update Home.js

--- Home.js
+++ Home.js
@@ -15,23 +15,23 @@ const Home = () => {
   }, [])
 
   return (
-    <View style={{flex: 1}}>
-    <View style={styles.homeMainBox}>
-      <View style={styles.homeBox}>
-        <Text style={styles.text}>Page 1</Text>
+    <View style={styles.homeBigBox}>
+      <View style={styles.homeMainBox}>
+        <View style={styles.homeBox}>
+          <Text style={styles.text}>Page 1</Text>
+        </View>
+        <View style={styles.homeBox}>
+          <Text style={styles.text}>Page 2</Text>
+        </View>
       </View>
-      <View style={styles.homeBox}>
-        <Text style={styles.text}>Page 2</Text>
+      <View style={styles.homeMainBox}>
+        <View style={styles.homeBox}>
+          <Text style={styles.text}>Page 3</Text>
+        </View>
+        <View style={styles.homeBox}>
+          <Text style={styles.text}>Page 4</Text>
+        </View>
       </View>
-    </View>
-    <View style={styles.homeMainBox}>
-      <View style={styles.homeBox}>
-        <Text style={styles.text}>Page 3</Text>
-      </View>
-      <View style={styles.homeBox}>
-        <Text style={styles.text}>Page 4</Text>
-      </View>
-    </View>
   </View>
   );
 };
