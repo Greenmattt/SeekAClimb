8416638bbc3f2e95121dfb5d6074f336df653716
Nico <100127990+NicolasAnglade63@users.noreply.github.com>
2023-03-03 15:21:15 +0100

    Changement de la route vers Home dans App.js

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import React, { useEffect , useState} from 'react';
 import Settings from './Settings/Main';
 import Account from './Account/Main';
 import Routes from './Routes/Main';
-import Home from './Home';
+import Home from './Home/Home';
 
 import style from './Component/Styles'
 
