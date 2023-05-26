f1e9ab0cbb8eeb1d27cfec3af5b7fb4ddc5111b8
Nico <100127990+NicolasAnglade63@users.noreply.github.com>
2023-05-26 15:52:00 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import { Text, View, Image, ActivityIndicator, TouchableOpacity, StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import React, { useEffect , useState} from 'react';
-import SSHClient from 'react-native-ssh-sftp';
 
 
 // pour la nav bar : npm install @react-navigation/material-top-tabs react-native-tab-view
@@ -16,54 +15,6 @@ import Home from './Home/Home';
 import style from './Component/Styles'
 
 const App = () => {
-
-  const envoiSFTP = () =>{
-    let client = new SSHClient('91.164.5.221', 50001, 'a_nicolas', '17/nico/06', (error) => {
-    if (error) console.warn(error);
-    });
-
-    client.connectSFTP((error) =>{
-      if (error) console.warn(error);
-    });
-
-    client.sftpMkdir(id, (error) => {
-      if (error) console.warn(error);
-    });
-
-    client.sftpUpload(imagepPath, imagePath, (error) => {
-      if (error) console.warn(error);
-    });
-
-    client.disconnectSFTP();
-
-  }
-
-  const [estHTTPenvoye, setEstHTTPenvoye] = useState(false);
-  const [estHTTPresolu, setEstHTTPresolu] = useState(false);
-  const [reponseHTTP, setReponseHTTP] = useState();
-  const [estErreurHTTP, setEstErreurHTTP] = useState(false);
-  
-
-  const envoiHTTP = async(id) => {
-    try { // là c'est si tout va bien
-    setEstHTTPenvoye(true);
-      let response = await fetch('http://91.164.5.221:50000/iaRoute?id='+String(id), {
-        method:'GET',
-      });
-      let json = await response.json();
-      setReponseHTTP(json);
-    } 
-    catch (error) { // là c'est si on a un pb
-      console.error(error);
-      setReponseHTTP("<Pas de réponse de l'API>");
-      setEstErreurHTTP(true);
-
-    }
-    finally { // là c'est une fois qu'on a vérifié qu'il n'y a pas d'erreur et que tout est fini
-      setEstHTTPresolu(true);
-      setEstErreurHTTP(false);
-    }
-  }
 
   // load du style
   const [styles, setLeStyle] = useState({});
@@ -142,11 +93,6 @@ const App = () => {
 
     : <ActivityIndicator sier="large" color="#0000ff"/>}
 
-  <View style={{position:'absolute', left :'82%', top:'82%'}}>
-    <TouchableOpacity style={styles.scanButton}>
-      <Image style={styles.scanIcon} source={require('./assets/ultra_icon.png')}/>
-    </TouchableOpacity>
-  </View>
 
     </View>
   );
