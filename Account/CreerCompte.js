a504a3a378f185939f9df389629ff2761d27f3d8
NicolasAnglade63 <100127990+NicolasAnglade63@users.noreply.github.com>
2023-05-29 22:39:59 +0200

    Création de compte presque fini
    Il reste quelques erreurs majeures et mineures à régler

--- Account/CreerCompte.js
+++ Account/CreerCompte.js
@@ -21,42 +21,87 @@ const CreerCompte = (props) => {
     getStyle();
   }, [])
 
-  const [textNom, onChangeNom] = useState('');
-  const [textPrenom, onChangePrenom] = useState('');
+  const [textPseudo, onChangePseudo] = useState('');
   const [textEmail, onChangeEmail] = useState('');
   const [textMdp, onChangeMdp] = useState('');
   const [textMdpConfirme, onChangeMdpConfirme] = useState('');
 
-  const [estMdpConcordant, setEstMdpConcordant] = useState(true);
+  const [estCompteAccepte, setEstCompteAccepte] = useState(false);
+  const [estCompteRefuse, setEstCompteRefuse] = useState(false);
+  const [textErreurRefus, setTextErreurRefus] = useState('');
+
+  const [estMailVerrifie, setEstMailVerrifie] = useState(false);
+
+  const [textCode, onChangeTextCode] = useState('');
 
   const creerCompte = async() => {
 
+    setEstCompteAccepte(false);
+    setEstCompteRefuse(false);
+
     if (textMdp != textMdpConfirme) {
-      setEstMdpConcordant(false);
-      console.log('different')
+      setEstCompteRefuse(false);
+      setTextErreurRefus("Les mots de passe ne concordent pas !");
     }
     else {
       try {
         let res = await fetch('http://91.164.5.221:50000/creerCompte',{
           method:'POST',
           body: JSON.stringify({
-            nom: textNom,
-            prenom: textPrenom,
+            pseudo: textPseudo,
             email: textEmail,
             mdp: textMdp
           }),
           headers: {'Content-Type':'application/json'}
         });
 
-        var texte = await res.text();
-        console.log(texte);
+        var json_res = await res.json();
 
       } catch(error) {
         console.warn(error);
+      } finally {
+        if (json_res.erreur != undefined) {
+          setEstCompteRefuse(true);
+          setTextErreurRefus(String(json_res.erreur));
+        }
+        else {
+          setEstCompteAccepte(true);
+        }
       }
-
-
     }
+  }
+
+  const validerCode = async() => {
+    try {
+      codeChiffre = ParseInt(textCode);
+    } catch(error) {
+      setEstCompteRefuse(true)
+      setTextErreurRefus("Le code n'est pas composé de chiffres")
+    }
+
+    try {
+      let res = await fetch('http://91.164.5.221:50000/verrifMail',{
+        method:'POST',
+        body: JSON.stringify({
+          email: textEmail,
+          code : codeChiffre
+        }),
+        headers: {'Content-Type':'application/json'}
+      });
+
+       var json_res = await res.json();
+
+        } catch(error) {
+          console.warn(error);
+        } finally {
+          if (json_res.erreur != undefined) {
+            setEstCompteRefuse(true);
+            setTextErreurRefus(String(json_res.erreur));
+          }
+          else {
+            setEstMailVerrifie(true);
+          }
+        }
   }
 
   return (
@@ -68,10 +113,7 @@ const CreerCompte = (props) => {
       <View style={styles.informationsBox}>
 
         {/*Input nom */}
-        <StrInput onChangeText={onChangeNom} value={textNom} placeholder={"Nom"} secureTextEntry={false}/>
-
-        {/*Input prénom */}
-        <StrInput onChangeText={onChangePrenom} value={textPrenom} placeholder={"Prenom"} secureTextEntry={false}/>
+        <StrInput onChangeText={onChangePseudo} value={textPseudo} placeholder={"Pseudo"} secureTextEntry={false}/>
 
         {/*Input email */}
         <StrInput onChangeText={onChangeEmail} value={textEmail} placeholder= {"Email"} secureTextEntry={false}/>
@@ -82,12 +124,30 @@ const CreerCompte = (props) => {
         {/*Input validation du mot de passe */}
         <StrInput onChangeText={onChangeMdpConfirme} value={textMdpConfirme} placeholder= {"Valider le mot de passe"} secureTextEntry={true}/>
 
-        {!estMdpConcordant ? <Text style={styles.text}>Les mots de passe ne concordent pas !</Text> : <View/>}
+        {estCompteRefuse ? <Text style={styles.text}>{textErreurRefus}</Text> : <View/>}
+
+        {estCompteAccepte ? 
+        <View style= {{flex:2}}> 
+          <View style={{flex:0.5}}>
+            <Text style={styles.text}>Vous allez recevoir un mail contenant le code à 6 chiffres permettant de verrifier votre comtpe dans les 5 prochaines minutes</Text>
+          </View>
+          <StrInput onChangeText={onChangeTextCode} value={textCode} placeholder = {"Code à 6 chiffres"} secureTextEntry={false}/> 
+          <View style = {{flex:0.5}}>
+            <TouchableOpacity style={styles.photoVerrifButton} onPress={validerCode}>
+              <Text style={styles.text}>Valider code</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        : <View/>}
+
+        {estMailVerrifie ? <Text style={styles.text}>Votre mail est verrifié, vous pouvez désormais vous connecter à votre compte dans la section : Se Connecter</Text>
+         : <View/>}
 
       </View>
 
       <TouchableOpacity style={styles.photoVerrifButton} onPress={creerCompte}>
-        <Text style={styles.text}>Valider</Text>
+        <Text style={styles.text}>Valider compte</Text>
       </TouchableOpacity>
     </View>
   );
