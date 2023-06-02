f46478cdb9f71404358522c7d5d4ea25f1b07fd2
Thechalk <100965196+Thechalk@users.noreply.github.com>
2023-06-02 15:18:43 +0200

    Update SeConnecter.js

--- Account/SeConnecter.js
+++ Account/SeConnecter.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import { View } from 'react-native';
+import { View, TouchableOpacity, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import style from '../Component/Styles';
 import StrInput from '../Component/StrInput';
@@ -13,6 +14,10 @@ const SeConnecter = (props) => {
   const [textEmail, onChangeEmail] = useState('');
   const [textMdp, onChangeMdp] = useState('');
 
+  const [estCompteRefuse, setEstCompteRefuse] = useState(false);
+  const [estCompteAccepte, setEstCompteAccepte] = useState(false);
+  const [textErreurRefus, setTextErreurRefus] = useState('');
+
   // load du style
   const [styles, setLeStyle] = useState({});
 
@@ -23,6 +28,36 @@ const SeConnecter = (props) => {
     }
     getStyle();
   }, [])
+
+const connecter = async() => {
+  try {
+    let res = await fetch('http://91.164.5.221:50000/Connect?email='+String(textEmail)+'&mdp='+String(textMdp),{
+      method:'GET',
+    });
+
+      var json_res = await res.json();
+
+  } catch(error) {
+    console.warn(error);
+  } finally {
+    if (json_res.erreur != undefined) {
+      setEstCompteRefuse(true);
+      setTextErreurRefus(String(json_res.erreur));
+    }
+    else {
+      if (json_res["201"] == "True") {
+        setEstCompteAccepte(true);
+        try {
+          await AsyncStorage.setItem("@email", JSON.stringify(textEmail));
+          await AsyncStorage.setItem("@mdp", JSON.stringify(textMdp));
+          console.log("email : ", textEmail, "mdp", textMdp);
+        } catch(e) {
+          console.warn(e);
+        }
+      }
+    }
+  }
+}
 
   return (
     <View style={styles.container}>
@@ -35,8 +70,16 @@ const SeConnecter = (props) => {
 
         {/*Input mot de passe */}
         <StrInput onChangeText={onChangeMdp} value={textMdp} placeholder={"Mot de passe"} secureTextEntry={false}/>
-      
+
       </View>
+      
+      <TouchableOpacity style={styles.photoVerrifButton} onPress={connecter}>
+        <Text style={styles.text}>Se Connecter</Text>
+      </TouchableOpacity>
+
+      {estCompteRefuse ? <Text style={styles.text}>{textErreurRefus}</Text> : <View/>}
+
+      {estCompteAccepte ? <Text style={styles.text}>Vous êtes connecté</Text>: <View/>}
     </View>
   );
 };
