d44982131483091ee01a01cb4bfd92234a7cc8a9
NicolasAnglade63 <100127990+NicolasAnglade63@users.noreply.github.com>
2023-06-06 22:25:53 +0200

    Miniatures et click de miniatures CreerBloc

--- Routes/CreerBloc.js
+++ Routes/CreerBloc.js
@@ -29,6 +29,48 @@ const CreerBloc = (props) => {
         getStyle();
     }, []);
 
+    const [miniatures, setMiniatures] = useState([]);
+
+    useEffect(() =>{
+        async function getMiniature (){
+            if (lieuPicker[0] != undefined) {
+                console.log(lieuPicker)
+                try {
+                    let response = await fetch('http://91.164.5.221:50000/Miniature?idSite='+String(lieuPicker));
+                    let json = await response.json();
+
+                    let liste_TO_res = [];
+                    
+                    Object.keys(json).forEach(function(key){
+                        liste_TO_res.push(
+                            <TouchableOpacity key={key} style={{width:100, height:100, backgroundColor:'#252525'}} onPress={() => getImage(json[key].IDSite, json[key].IDMur, json[key].numeroImage)}>
+                                <Image style={{flex:1}} resizeMode='contain' source={{uri: 'data:image/png;base64,'+json[key].imageB64}}/>
+                            </TouchableOpacity>
+                        );
+                    });
+
+                    setMiniatures(liste_TO_res);
+
+                }catch(e){
+                    console.warn(e);
+                }
+            }
+        }
+        getMiniature();
+
+    }, [lieuPicker]);
+
+    const getImage = async(idSite, idMur, numeroImage) => {
+        try {
+            let response = await fetch('http://91.164.5.221:50000/getImage?idSite='+idSite+"&idMur="+idMur+"&numeroImage="+numeroImage);
+            let texte = await response.text();
+            console.log('charge');
+        } catch(e) {
+            console.warn(e)
+        }
+
+    }
+
     return (
         <View style={styles.container}>
             <GoBackButton onPress = {() => GoBackToRoutes(props)}/>
@@ -49,6 +91,8 @@ const CreerBloc = (props) => {
                 style={{borderWidth: 0}}/>
 
                 <Text style={styles.text}>Choisissez l'image sur laquelle est votre route :</Text>
+
+                {miniatures}
             </View>
         </View>
     );
